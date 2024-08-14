d6663c44d1c2f2edcc80616a6c54ff8edaa8c25b
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-08-14 12:39:16 +0530

    update backend

--- src/routes/songRoute.js
+++ src/routes/songRoute.js
@@ -1,9 +1,10 @@
 import { addsong,listSong } from "../controllers/songsController.js";
 import express from 'express'
+import upload from "../middleware/mulder.js";
 
 const songRouter =express.Router();
 
-songRouter.post('/add',addsong);
+songRouter.post('/add',upload.fields([{name:'image',maxCount:1},{name:'audio',maxCount:1}]),addsong);
 songRouter.get('/list',listSong);
 
 export default songRouter;
